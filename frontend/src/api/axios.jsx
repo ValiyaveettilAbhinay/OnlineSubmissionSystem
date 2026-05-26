305a6e59f6188f8c07ace07b27a27f443c99446a
ValiyaveettilAbhinay <chinnuchikku35@gmail.com>
2026-05-26 18:43:41 +0530

    added api to url

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://onlinesubsytem-backend.onrender.com',
+  baseURL: 'https://onlinesubsytem-backend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
